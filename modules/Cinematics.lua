916e09dee379c2f73f109cb261d194e9c92064ee
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-07-24 21:42:52 +0200

    Change cinematic registration for debugging purposes

--- modules/Cinematics.lua
+++ modules/Cinematics.lua
@@ -12,22 +12,26 @@ app.Event:Register("ADDON_LOADED", function(addOnName, containsBindings)
 	if addOnName == appName then
 		app.Settings.cinematics = app.Settings.cinematics or {}
 
+		local function registerMovie(movieID)
+			app.Settings.cinematics[movieID] = app.Settings.cinematics[movieID] or {}
+			local date = C_DateAndTime.GetCurrentCalendarTime()
+			local mapID = C_Map.GetBestMapForUnit("player")
+			table.insert(app.Settings.cinematics[movieID], { dateTime = date.year .. "-" .. date.month .. "-" .. date.monthDay .. " " .. date.hour .. ":" .. date.minute, map = C_Map.GetMapInfo(mapID).name, mapID = mapID })
+		end
+
 		local function handleMovie(movieID, source)
 			app:Debug(source)
 			if not movieID then
 				app:Debug("No movieID found")
 			elseif app.Settings.cinematics[movieID] then
 				app:Debug("Skipped movieID", movieID)
+				C_Timer.After(2, function() registerMovie(movieID) end)
 				-- MovieFrame:Hide()
 				-- StopCinematic()
 				-- CancelScene()
 			elseif movieID then
-				C_Timer.After(2, function()
-					app:Debug("Registering movieID", movieID)
-					local date = C_DateAndTime.GetCurrentCalendarTime()
-					local mapID = C_Map.GetBestMapForUnit("player")
-					app.Settings.cinematics[movieID] = date.year .. "-" .. date.month .. "-" .. date.monthDay .. " " .. date.hour .. ":" .. date.minute .. " " .. format("%s (%d)", C_Map.GetMapInfo(mapID).name, mapID)
-				end)
+				app:Debug("Registering movieID", movieID)
+				C_Timer.After(2, function() registerMovie(movieID) end)
 			end
 		end
 
